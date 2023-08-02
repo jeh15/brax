3a998aac8dc9ca501af9726a97f3b748ee134df1
jeh15 <jeh15@fsu.edu>
2023-08-02 04:05:01 -0400

    Revert "Changed input for implicit diff of pg solution"
    This reverts commit 99672d1120d070db7bcbc340fdd41e80140f135f.

--- brax/generalized/constraint.py
+++ brax/generalized/constraint.py
@@ -227,7 +227,7 @@ def force(sys: System, state: State) -> jp.ndarray:
   # fairly sparse.  perhaps worth trying some kind of random or
   # learned projection to solve a smaller dense matrix at each step
 
-  def solve(input):
+  def solve(b):
     pg = jaxopt.ProjectedGradient(
         objective,
         jaxopt.projection.projection_non_negative,
@@ -235,11 +235,10 @@ def force(sys: System, state: State) -> jp.ndarray:
         implicit_diff=True,
         maxls=sys.solver_maxls,
     )
-    qf_constraint = state.con_jac.T @ pg.run(input).params
+    qf_constraint = state.con_jac.T @ pg.run(jp.zeros_like(b)).params
     return qf_constraint
 
-  input = jp.zeros_like(b)
-  qf_constraint = solve(input)
+  qf_constraint = solve(b)
 
   return qf_constraint
 
